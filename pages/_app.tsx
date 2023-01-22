1fecfb268cbc65be86f69f6109e25111d043e2e6
Julien CORNILLE <julien_cornille@carrefour.com>
2023-01-22 19:32:07 +0100

    feat: enable dark themes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,27 @@
 import "../styles/globals.css";
 // pages/_app.js
 import { Roboto } from "@next/font/google";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
 
 const roboto = Roboto({
   weight: "400",
   subsets: ["latin"],
 });
 
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
 export default function App({ Component, pageProps }) {
   return (
-    <main className={roboto.className}>
-      <Component {...pageProps} />
-    </main>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <main className={roboto.className}>
+        <Component {...pageProps} />
+      </main>
+    </ThemeProvider>
   );
 }
